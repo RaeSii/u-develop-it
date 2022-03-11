56a8378c8fcd88ab61284b463e2dfbf8a1c8bee6
raesii <leisolo@gmail.com>
2022-03-10 20:33:17 -0800

    added git ignore file

---
+++ utils/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+// Express Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+
+  app.get('/', (req, res) => {
+      res.json({
+          message: 'Hello, Mamani'
+      });
+  });
