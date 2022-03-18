e22ffbfbd476b84c764b942b7aef1f80ae2a9b98
raesii <leisolo@gmail.com>
2022-03-18 16:54:51 -0700

    passed test, added server.js and ran npm start

--- server.js
+++ server.js
@@ -14,3 +14,7 @@ app.listen(PORT, () => {
           message: 'Hello, Mamani'
       });
   });
+  // Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+  });
